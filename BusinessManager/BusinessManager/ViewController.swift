c0523ba22508a962e31acb5e7c7124b9fb8ac600
Rahul Soni <rsoni@xactlycorp.com>
2017-06-07 10:58:19 +0530

    Commit - 1 - O

---
+++ BusinessManager/BusinessManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BusinessManager
+//
+//  Created by Rahul Soni on 5/21/17.
+//  Copyright © 2017 Rahul Soni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
